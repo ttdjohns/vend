6a4f2003d064fd0513b0a55863f3480f1f18a433
ttdjohns <32712899+ttdjohns@users.noreply.github.com>
2017-10-12 12:24:57 -0600

    Add files via upload

---
+++ Deliverable.java
@@ -0,0 +1,10 @@
+package org.lsmr.vending;
+
+/**
+ * Marker interface representing that instances of implementing classes can be
+ * delivered by the vending machine.
+ * 
+ * @author Robert J. Walker
+ */
+public interface Deliverable {
+}
